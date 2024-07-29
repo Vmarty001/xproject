3a988c57394301668809d8687cb1679cc6f1c365
vmay <va>
2024-07-29 04:40:42 +0300

    fix some 0.4

--- src/components/Points/Points.jsx
+++ src/components/Points/Points.jsx
@@ -16,7 +16,7 @@ const Points = () => {
         alert(`User username: ${user?.username}`);
       
         try {
-          const response = await fetch('https://109.196.164.164:3000/add-user', {
+          const response = await fetch('https://185.135.149.43:3000/add-user', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
